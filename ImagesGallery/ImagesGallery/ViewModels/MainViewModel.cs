fa60136c77d03ee36c749c2200f63fe3ed04fb1b
Leandro Bentes <leandro.bentes@indt.org.br>
2019-12-15 23:08:30 -0400

    [Setup] Initial WPF project
    Signed-off-by: Leandro Bentes <leandro.bentes@indt.org.br>

---
+++ ImagesGallery/ImagesGallery/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace ImagesGallery.ViewModels
+{
+    class MainViewModel : PropertyChangedBase
+    {
+        private string _windowTitle = "ImagesGallery";
+        public string WindowTitle
+        {
+            get { return _windowTitle; }
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+    }
+}
